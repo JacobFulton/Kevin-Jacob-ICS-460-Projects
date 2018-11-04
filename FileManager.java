92443aba2c8db766eaafce9c0db039c067465c73
JacobFulton <35611763+JacobFulton@users.noreply.github.com>
2018-11-03 21:10:47 -0500

    Add files via upload

--- FileManager.java
+++ FileManager.java
@@ -7,59 +7,59 @@ import java.util.*;
 
 public class FileManager {
 
-	private File file;
-	private int packetLength;
-	private int index;
-	private int packetCount;
-	byte[] fileContent = null;
-	private int headerLength;
+    private File file;
+    private int packetLength;
+    private int index;
+    private int packetCount;
+    byte[] fileContent = null;
+    private int headerLength;
 
-	public FileManager(int packetLength) {
-	    headerLength = 2;
-		index = 0;
-		packetCount = 0;
-		this.packetLength = packetLength;
-	}
+    public FileManager(int packetLength) {
+        headerLength = 2;
+        index = 0;
+        packetCount = 0;
+        this.packetLength = packetLength;
+    }
 
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public int getPacketLength() {
-		return packetLength;
-	}
+    public int getPacketLength() {
+        return packetLength;
+    }
 
-	public int getPacketCount() {
-		return packetCount;
-	}
+    public int getPacketCount() {
+        return packetCount;
+    }
 
-	public void setPacketCount(int packetCount) {
-		this.packetCount = packetCount;
-	}
+    public void setPacketCount(int packetCount) {
+        this.packetCount = packetCount;
+    }
 
     public byte[] importFile(String path) {
-    	file = new File(path);
-    	try {
-			fileContent = Files.readAllBytes(file.toPath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	return fileContent;
+        file = new File(path);
+        try {
+            fileContent = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return fileContent;
     }
 
     public void exportFile(String path) {
-    	try (FileOutputStream outputStream = new FileOutputStream(path)) {
-    		   outputStream.write(fileContent);
+        try (FileOutputStream outputStream = new FileOutputStream(path)) {
+               outputStream.write(fileContent);
 
-    		} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
     }
 
     public boolean isCorrupted(byte[] packet){
@@ -71,75 +71,75 @@ public class FileManager {
 
     public byte[] extractPacketHeader(byte[] packet){
 
-        byte[] header = Arrays.copyOfRange(packet, 0, headerLength-1); //Copies header for header size of 16
+        byte[] header = Arrays.copyOfRange(packet, 0, headerLength); //Copies header
         return header;
     }
 
     public byte[] extractPacket(byte[] packet){
 
-        byte[] header = Arrays.copyOfRange(packet, headerLength, packet.length);//Copies packet after header
-        return header;
+        byte[] pac = Arrays.copyOfRange(packet, headerLength, packet.length+headerLength);//Copies packet after header
+        return pac;
     }
 
 
-    public void addPacket(byte[] packet) {
+    public boolean addPacket(byte[] packet) {
         if(isCorrupted(packet) || packet[1] != (byte) packetCount){
-            return; //does not add packet if the packet is corrupted or if it's not the next packet.
+            return false; //does not add packet if the packet is corrupted or if it's not the next packet.
         }
-    	int pStart = index;
-    	int pEnd = index+packet.length-1;
+        byte[] newPacket = extractPacket(packet);
+        int pStart = index;
+        int pEnd = index+newPacket.length-1;
 
-    	System.out.println("R["+(packetCount+1)+"]-["+pStart+"]-["+pEnd+"]");
-    	//System.out.println(index);
+        System.out.println("R[" + (packetCount+1) + "]-[" + pStart + "]-[" + pEnd + "]");
+        //System.out.println(index);
 
-    	if(fileContent == null) {
-    		fileContent = new byte[1024];
-    	}else {
-    	byte[] temp = new byte[index+packet.length];
-    	System.arraycopy(fileContent, 0, temp, 0, index);
-    	fileContent = temp;
-    	//System.out.println(fileContent.length);
-    	}
+        if(fileContent == null) {
+            fileContent = new byte[1024];
+        }else {
+        byte[] temp = new byte[index + newPacket.length];
+        System.arraycopy(fileContent, 0, temp, 0, index);
+        fileContent = temp;
+        //System.out.println(fileContent.length);
+        }
 
-    	byte[] temp = new byte[fileContent.length + packet.length];
+        byte[] temp = new byte[fileContent.length + newPacket.length];
 
-    	System.arraycopy(fileContent, 0, temp, 0, fileContent.length);
+        System.arraycopy(fileContent, 0, temp, 0, fileContent.length);
 
-    	System.arraycopy(packet, 0, temp, 0, packet.length);
+        System.arraycopy(newPacket, 0, temp, index, newPacket.length);
 
-    	fileContent = temp;
-    	//for(int i = 0; i < packet.length; i++) {
-    	//	fileContent[i+index] = packet[i];
-    	//}
-    	index = index + packet.length;
-    	packetCount++;
+        fileContent = temp;
+
+        index = index + newPacket.length;
+        packetCount++;
+        return true;
     }
 
     public byte[] nextPacket() {
-    	int pStart = index;
+        int pStart = index;
 
-    	if((index+packetLength) > fileContent.length){
-    		packetLength = fileContent.length - index;
-    	}else if((index == fileContent.length)) {
-    		byte[] end = null;
-    		return end;
-    	}
+        if((index+packetLength) > fileContent.length){
+            packetLength = fileContent.length - index;
+        }else if((index == fileContent.length)) {
+            byte[] end = null;
+            return end;
+        }
 
-    	int pEnd = index+packetLength-1;
+        int pEnd = index+packetLength-1;
 
-    	System.out.println("S["+(packetCount+1)+"]-["+pStart+"]-["+pEnd+"]");
+        System.out.println("S["+(packetCount+1)+"]-["+pStart+"]-["+pEnd+"]");
 
-    	byte[] packet = new byte[packetLength+headerLength];
+        byte[] packet = new byte[packetLength+headerLength];
 
-    	System.arraycopy(fileContent, index, packet, headerLength-1, packetLength);
-    	if(Math.random() < 0.1)
-    	    packet[0] = (byte) -128; //corrupts about 1 in 10 packets, on average.
-    	packet[1] = (byte) packetCount;
-    	//for(int i = 0; i < packetLength; i++) {
-    	//	packet[i] = fileContent[index];
-    	//	index++;
-    	//}
-    	packetCount++;
-    	return packet;
+        System.arraycopy(fileContent, index, packet, headerLength, packetLength);
+        index = index + packetLength;
+        packet[0] = (byte) 0;
+        //if(Math.random() < 0.1)
+          //  packet[0] = (byte) -128; //corrupts about 1 in 10 packets, on average.
+        packet[1] = (byte) packetCount;
+
+
+        packetCount++;
+        return packet;
     }
 }
